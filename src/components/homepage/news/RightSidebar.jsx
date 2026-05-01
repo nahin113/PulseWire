f16f555a4a6832c1d36192a475aaae92c4e60ea2
nahin113 <nahinahmed113@gmail.com>
2026-05-02 02:56:59 +0600

    feat: secure routes via proxy middleware and extend auth with Google and GitHub OAuth providers

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,16 +1,38 @@
+"use client";
+
+import { createAuthClient } from "better-auth/client";
 import { FaGoogle, FaGithub } from "react-icons/fa";
 
+// Initialize client after imports
+const authClient = createAuthClient();
+
 const RightSidebar = () => {
+  const handleGoogleSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+    console.log(data);
+  }; 
+  const handleGithubSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+    console.log(data);
+  }; 
+
   return (
     <div>
-      <h2 className="font bold text-lg mb-4">Login with</h2>
+      <h2 className="font-bold text-lg mb-4">Login with</h2>
       <div className="flex flex-col gap-2">
-      <button className="btn border-blue-500 text-blue-500">
-        <FaGoogle></FaGoogle> Login with Google
-      </button>
-      <button className="btn border-black text-black">
-        <FaGithub></FaGithub> Login with Github
-      </button>
+        <button
+          onClick={handleGoogleSignIn}
+          className="btn border-blue-500 text-blue-500"
+        >
+          <FaGoogle /> Login with Google
+        </button>
+        <button onClick={handleGithubSignIn} className="btn border-black text-black">
+          <FaGithub /> Login with Github
+        </button>
       </div>
     </div>
   );
